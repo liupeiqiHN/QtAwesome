adc1d184581aa736261395bcfcc9eb77e017a93b
刘佩其 <liupeiqi@live.cn>
2022-05-07 15:32:40 +0800

    build with visual studio and qt vs tools

--- QtAwesomeSample/main.cpp
+++ QtAwesomeSample/main.cpp
@@ -13,51 +13,51 @@
 #include <QVBoxLayout>
 #include <QWidget>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    QMainWindow w;
+	QApplication app(argc, argv);
+	QMainWindow w;
 
-    QtAwesome* awesome = new QtAwesome(&w);
-    awesome->initFontAwesome();
+	QtAwesome* awesome = new QtAwesome(&w);
+	awesome->initFontAwesome();
 
-    QVBoxLayout* layout = new QVBoxLayout();
+	QVBoxLayout* layout = new QVBoxLayout();
 
-    // a simple beer button
-    //=====================
-    {
-        QPushButton* beerButton = new QPushButton( "Cheers!");
+	// a simple beer button
+	//=====================
+	{
+		QPushButton* beerButton = new QPushButton("Cheers!");
 
-        QVariantMap options;
-        options.insert("anim", qVariantFromValue( new QtAwesomeAnimation(beerButton) ) );
-        beerButton->setIcon( awesome->icon( fa::beer, options  ) );
+		QVariantMap options;
+		options.insert("anim", QVariant::fromValue(new QtAwesomeAnimation(beerButton)));
+		beerButton->setIcon(awesome->icon(fa::beer, options));
 
-        layout->addWidget(beerButton);
-    }
+		layout->addWidget(beerButton);
+	}
 
-    // a simple beer checkbox button
-    //==============================
-    {
-        QPushButton* toggleButton = new QPushButton("Toggle Me");
-        toggleButton->setCheckable(true);
+	// a simple beer checkbox button
+	//==============================
+	{
+		QPushButton* toggleButton = new QPushButton("Toggle Me");
+		toggleButton->setCheckable(true);
 
-        QVariantMap options;
-        options.insert("color", QColor(Qt::green) );
-        options.insert("text-off", QString(fa::squareo) );
-        options.insert("color-off", QColor(Qt::red) );
-        toggleButton->setIcon( awesome->icon( fa::checksquareo, options ));
-
-
-        layout->addWidget(toggleButton);
-    }
+		QVariantMap options;
+		options.insert("color", QColor(Qt::green));
+		options.insert("text-off", QString(QChar(fa::squareo)));
+		options.insert("color-off", QColor(Qt::red));
+		toggleButton->setIcon(awesome->icon(fa::checksquareo, options));
 
 
-    // add the samples
-    QWidget* samples = new QWidget();
-    samples->setLayout(layout);
-    w.setCentralWidget(samples);
+		layout->addWidget(toggleButton);
+	}
 
-    w.show();
-    
-    return app.exec();
+
+	// add the samples
+	QWidget* samples = new QWidget();
+	samples->setLayout(layout);
+	w.setCentralWidget(samples);
+
+	w.show();
+
+	return app.exec();
 }
